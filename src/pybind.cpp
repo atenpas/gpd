86034314243b7a3c8346448edf5b65ed93229cd9
Patrick Naughton <patricknaughton01@gmail.com>
2022-01-31 10:25:08 -0600

    Add support for visibility and mult cam transforms

--- src/pybind.cpp
+++ src/pybind.cpp
@@ -17,9 +17,17 @@
 // 3 for position, 9 for orientation, 1 for width, 1 for score
 #define GRASP_DESC_LEN 14
 
+/**
+ *  Detect grasps from a given point cloud
+ *  pcd: (n,3), each row is a point in the cloud
+ *  visibility: (num_cams, n), each row is a camera, each column j denotes if
+ *      the corresponding point is visible from the given camera.
+ *  camera: (3, num_cams), each column j is the position of the camera.
+ */
 pybind11::array_t<double> detectGrasps(
     gpd::GraspDetector &detector,
     pybind11::array_t<double, pybind11::array::c_style> pcd,
+    pybind11::array_t<double, pybind11::array::c_style> visibility,
     pybind11::array_t<double, pybind11::array::c_style> camera)
 {
     if (pcd.ndim() != 2) {
@@ -32,6 +40,7 @@ pybind11::array_t<double> detectGrasps(
     if (pcd.shape()[1] < 3) {
         throw std::invalid_argument("Each point must have at least 3 coordinates");
     }
+    ssize_t num_cams = camera.shape()[1];
     pcl::PointCloud<pcl::PointXYZRGBA> e_cloud;
     pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_ptr = e_cloud.makeShared();
     for(ssize_t i = 0; i < len; i++){
@@ -41,12 +50,19 @@ pybind11::array_t<double> detectGrasps(
         pt.z = pcd.at(i, 2);
         cloud_ptr->push_back(pt);
     }
-    Eigen::MatrixXi visibility = Eigen::MatrixXi::Ones(1, len);
-    Eigen::Matrix3Xd view_points(3,1);
-    for(ssize_t i = 0; i < 3; i++){
-        view_points(i) = camera.at(i);
+    Eigen::MatrixXi vis(num_cams, len);
+    for(ssize_t i = 0; i < num_cams; i++){
+        for (ssize_t j = 0; j < len; j++){
+            vis(i, j) = visibility.at(i, j);
+        }
     }
-    gpd::util::Cloud gpd_cloud(cloud_ptr, visibility, view_points);
+    Eigen::Matrix3Xd view_points(3, num_cams);
+    for(ssize_t i = 0; i < 3; i++){
+        for(ssize_t j = 0; j < num_cams; j++){
+            view_points(i, j) = camera.at(i, j);
+        }
+    }
+    gpd::util::Cloud gpd_cloud(cloud_ptr, vis, view_points);
     detector.preprocessPointCloud(gpd_cloud);
     std::vector<std::unique_ptr<gpd::candidate::Hand>> grasps = detector.detectGrasps(gpd_cloud);
     std::vector<std::array<double, GRASP_DESC_LEN>> ret;
--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from klampt.io import numpy_convert
 from klampt import vis
 
 detector = gpd.GraspDetector("cfg/eigen_params.cfg")
-pcd = o3d.io.read_point_cloud("save.pcd")
+pcd = o3d.io.read_point_cloud("tutorials/krylon.pcd")
 pts = np.asarray(pcd.points)
 pts = pts[np.logical_not(np.isnan(pts[:, 0])), :]
 # tf = np.array([
@@ -24,7 +24,7 @@ pts = pts[np.logical_not(np.isnan(pts[:, 0])), :]
 tf = np.eye(4)
 pts = (tf @ (np.hstack(( pts, np.ones((len(pts), 1)) ))).T).T[:, :3]
 offset = np.mean(pts, axis=0, keepdims=True)
-grasps = gpd.detectGrasps(detector, pts - offset, np.zeros(3))
+grasps = gpd.detectGrasps(detector, pts - offset, np.ones((1, len(pts))), np.zeros((3, 1)))
 print(grasps)
 k_pcd = klampt.PointCloud()
 for i in range(len(pts)):
